9fc35dd7f42bc65557f78c3f741eebd1c1459be2
Andre Girin <Andre.Girin@hardis.fr>
2018-03-08 17:26:05 +0100

    tableau contrat

--- appli_gestion_client-recherche/JS/load_formulaire.js
+++ appli_gestion_client-recherche/JS/load_formulaire.js
@@ -257,7 +257,7 @@ $(function(){
 function CDS(){
 	//récuperer le CDS du client
 
-	function getContratWMS(){
+	function getContactWMS(){
 		var requete = 'SELECT ctc FROM Contact AS ctc ';
 		requete += 'JOIN lnkContactToContract AS link ON link.contact_id = ctc.id ';
 		requete += 'JOIN Contract AS ctr ON link.contract_id = ctr.id ';
@@ -271,7 +271,7 @@ function CDS(){
 	wmsJSON = {
 		operation: 'core/get',
 		'class': 'Contact',
-		key: getContratWMS(),
+		key: getContactWMS(),
 		output_fields: "name"
 	}
 
@@ -293,12 +293,121 @@ function CDS(){
 			}else{
 				cds="PAS DE CDS";
 			}
-			testOK();
+			contractClient();
 		},
 		error: function(data){
 			console.log("cds error");
 		}
 	});
+}
+
+function contractClient(){
+	function getContratWMS(){
+		var requete = 'SELECT CustomerContract AS cc ';
+		requete += 'JOIN Organization AS org ON cc.org_id=org.id ';
+		requete += 'WHERE org.name="'+nomOrg+'"';
+		return requete;
+	}
+
+	contratJSON = {
+		operation: 'core/get',
+		'class': 'CustomerContract',
+		key: getContratWMS(),
+		output_fields: "name, status, provider_name, auto_close_ticket, closure_delay, start_date, end_date"
+	}
+
+	$.ajax({
+		type: "GET",
+		url: ITOP_WS_URL,
+		dataType: 'jsonp',
+		data: { auth_user: login, auth_pwd: pwd, json_data: JSON.stringify(contratJSON)},
+		crossDomain: 'true',
+		success: function(data){
+			$(".body-contrat").html("");
+			if(data == null){
+				$(".table-contrat").hide();
+				$(".contrat").html("Pas de contrat avec ce client.")
+			}else{
+				var rep = data['objects'];
+				/*console.log(data);
+				console.log(rep);*/
+				$.each(rep, function(index, value){
+					//console.log("contrat : "+value['fields']['name']);
+					var tr_contrat = document.createElement("tr");
+					var nom = document.createElement("td");
+					var statut = document.createElement("td");
+					var fournisseur = document.createElement("td");
+					var cloture_auto = document.createElement("td");
+					var delai = document.createElement("td");
+					var debut = document.createElement("td");
+					var fin = document.createElement("td");
+
+					var tr_tab_service = document.createElement("tr");
+					var tab_service = document.createElement("table");
+					var head = document.createElement("thead");
+					var tr_head_service = document.createElement("tr");
+					var service = document.createElement("th");
+					var heure = document.createElement("th");
+					var sla = document.createElement("th");
+					var body = document.createElement("tbody");
+
+					$(tab_service).addClass("table table-bordered table-service");
+					$(tr_head_service).addClass("thead-light");
+					$(tr_contrat).addClass("contrat-row");
+
+					$(nom).html(value['fields']['name']);
+					$(statut).html(value['fields']['status']);
+					$(fournisseur).html(value['fields']['provider_name']);
+					$(cloture_auto).html(value['fields']['auto_close_ticket']);
+					$(delai).html(value['fields']['closure_delay']);
+					$(debut).html(value['fields']['start_date']);
+					$(fin).html(value['fields']['end_date']);
+
+					$(tr_tab_service).html("<td colspan='7' class='td_tab_service'></td>");
+
+					$(tr_tab_service).hide();
+
+					$(service).html("Service");
+					$(heure).html("Heures ouvrées");
+					$(sla).html("SLA");
+
+					$(".body-contrat").append(tr_contrat);
+					$(tr_contrat).append(nom);
+					$(tr_contrat).append(statut);
+					$(tr_contrat).append(fournisseur);
+					$(tr_contrat).append(cloture_auto);
+					$(tr_contrat).append(delai);
+					$(tr_contrat).append(debut);
+					$(tr_contrat).append(fin);
+					$(tr_contrat).after(tr_tab_service);
+
+
+					$(tr_tab_service).find(".td_tab_service").append(tab_service);
+					$(tab_service).append(head);
+					$(head).append(tr_head_service);
+					$(tr_head_service).append(service);
+					$(tr_head_service).append(heure);
+					$(tr_head_service).append(sla);
+					$(tab_service).append(body);
+				});
+
+				$('.contrat-row').hover(function() {
+        	$(this).css('cursor','pointer');
+    		});
+
+				$(".contrat-row").click(function(){
+					$(this).next("tr").toggle();
+				});
+
+			}
+
+
+		},
+		error: function(data){
+			console.log("contrat error");
+		}
+	});
+	testOK();
 }
 
 
